673e10a65f7cfd471594f8bc47d2d1a3454a1bae
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-06-21 18:32:48 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
-
+server '13.113.23.214', user: 'ec2-user', roles: %w{app db web}
 
 # role-based syntax
 # ==================
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '13.113.23.214', user: 'ec2-user', roles: %w{app db web}
+
