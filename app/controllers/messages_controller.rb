91cc84a78800a6f8e6bd1127d2580e009e69cf61
神谷啓明 <kamiyahiroaki@kamiyahiroakinoMacBook-ea.local>
2019-09-11 17:07:00 +0900

    メッセージ画面遷移を制限

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,7 @@ class MessagesController < ApplicationController
   before_action :set_messages, only: [:index, :create]
   before_action :set_host, only: [:index, :create]
   before_action :set_attender, only: [:index, :create]
+  before_action :move_to_index, only: [:index, :create]
 
   def index
     @message = Message.new
@@ -40,5 +41,16 @@ class MessagesController < ApplicationController
 
   def set_attender
     @restaurantuser = UserRestaurant.where(restaurant_id: @restaurant)
+  end
+
+  def move_to_index
+    if user_signed_in?
+      if UserRestaurant.exists?(attender_id: current_user.id, restaurant_id: @restaurant.id)||Restaurant.exists?(host_id: current_user.id)
+      else
+        redirect_to root_path
+      end
+    else
+      redirect_to root_path
+    end
   end
 end
